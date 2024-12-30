2098cc6fafecfeef01c673dd3e6086519136b2fe
SurajsinghBayas <surajyou24@gmail.com>
2024-12-30 06:40:32 +0530

    Code added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Portfolio
-A sleek and modern portfolio website showcasing my projects, skills, and professional journey
+# Portfolio Webpage Example
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/leonam-silva-de-souza/pen/vYowKqP](https://codepen.io/leonam-silva-de-souza/pen/vYowKqP).
+
+Source: codewithsadee (https://www.youtube.com/watch?v=SoxmIlgf2zM)
---
+++ src/script.js
@@ -0,0 +1,90 @@
+'use strict';
+
+//Opening or closing side bar
+
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+sidebarBtn.addEventListener("click", function() {elementToggleFunc(sidebar); })
+
+//Activating Modal-testimonial
+
+//Activating Filter Select and filtering options
+
+const select = document.querySelector('[data-select]');
+const selectItems = document.querySelectorAll('[data-select-item]');
+const selectValue = document.querySelector('[data-select-value]');
+const filterBtn = document.querySelectorAll('[data-filter-btn]');
+
+select.addEventListener('click', function () {elementToggleFunc(this); });
+
+for(let i = 0; i < selectItems.length; i++) {
+    selectItems[i].addEventListener('click', function() {
+
+        let selectedValue = this.innerText.toLowerCase();
+        selectValue.innerText = this.innerText;
+        elementToggleFunc(select);
+        filterFunc(selectedValue);
+
+    });
+}
+
+const filterItems = document.querySelectorAll('[data-filter-item]');
+
+const filterFunc = function (selectedValue) {
+    for(let i = 0; i < filterItems.length; i++) {
+        if(selectedValue == "all") {
+            filterItems[i].classList.add('active');
+        } else if (selectedValue == filterItems[i].dataset.category) {
+            filterItems[i].classList.add('active');
+        } else {
+            filterItems[i].classList.remove('active');
+        }
+    }
+}
+
+//Enabling filter button for larger screens 
+
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+    filterBtn[i].addEventListener('click', function() {
+        let selectedValue = this.innerText.toLowerCase();
+        selectValue.innerText = this.innerText;
+        filterFunc(selectedValue);
+
+        lastClickedBtn.classList.remove('active');
+        this.classList.add('active');
+        lastClickedBtn = this;
+    });
+}
+
+// Enabling Contact Form
+
+
+  
+  
+
+// Enabling Page Navigation 
+
+const navigationLinks = document.querySelectorAll('[data-nav-link]');
+const pages = document.querySelectorAll('[data-page]');
+
+for(let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener('click', function() {
+        for(let j = 0; j < pages.length; j++) {
+            if(this.innerHTML.toLowerCase() == pages[j].dataset.page) {
+                pages[j].classList.add('active');
+                navigationLinks[j].classList.add('active');
+                window.scrollTo(0, 0);
+            } else {
+                pages[j].classList.remove('active');
+                navigationLinks[j].classList.remove('active');
+            }
+        }
+    });
+}
+
+  
